a99427a880ce269e4d3fa6e6693812b8bfcf9a98
ssmhdssmhd <ssmhdssmhd@users.noreply.github.com>
2026-07-23 21:29:58 +0000

    chore: 更新版本编码，增加 build 号和 version_code

--- version.php
+++ version.php
@@ -1,6 +1,8 @@
 <?php
 return [
     'version' => 'v5.8.4',
-    'commit' => 'v5.8.4',
+    'build' => '20260723',
+    'version_code' => 50804,
+    'commit' => '7151672',
     'updated_at' => '2026-07-23',
 ];
